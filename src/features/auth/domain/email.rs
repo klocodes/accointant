e2f335761fe43362955e35b355987e57c5698411
Дмитрий Клоков <klokov.dmitriy@gmail.com>
2024-01-24 19:06:10 +0600

    Отрефакторить Email value object

---
+++ src/features/auth/domain/email.rs
@@ -0,0 +1,35 @@
+use serde::{Deserialize, Serialize};
+use validator::Validate;
+use crate::errors::client::ClientErrors::{DomainError};
+use crate::errors::Error;
+
+#[derive(Debug, Clone, Serialize, Deserialize, Validate)]
+pub struct Email {
+    #[validate(email)]
+    #[serde(rename = "email")]
+    value: String,
+}
+
+impl Email {
+    pub fn new(email: &str) -> Result<Self, Error> {
+        let email = Self {
+            value: email.to_string()
+        };
+
+        if let Err(e) = email.validate() {
+            return Err(
+                Error::Client(
+                    DomainError {
+                        message: e.to_string().into()
+                    }
+                )
+            );
+        }
+
+        Ok(email)
+    }
+
+    pub fn value(&self) -> &str {
+        &self.value
+    }
+}
