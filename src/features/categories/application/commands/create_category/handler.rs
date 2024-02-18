473010fc4542566b9aaaf47c48b1311c224e3643
Дмитрий Клоков <klokov.dmitriy@gmail.com>
2024-02-18 10:10:20 +0600

    Write unit tests for category creating

--- src/features/categories/application/commands/create_category/handler.rs
+++ src/features/categories/application/commands/create_category/handler.rs
@@ -62,3 +62,74 @@ impl<R> CommandHandler<CreateCategoryCommand> for CreateCategoryCommandHandler<R
         )
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use futures_util::FutureExt;
+    use uuid::Uuid;
+    use crate::errors::server::ServerErrors::InternalServerError;
+    use crate::features::categories::domain::category_repository::MockCategoryRepository;
+    use super::*;
+
+    #[tokio::test]
+    async fn test_create_category_command_handler_success() {
+        let mut rep = MockCategoryRepository::new();
+        rep.expect_exists()
+            .times(1)
+            .returning(|_, _, _| {
+                async move {
+                    Ok(false) // Симулируем асинхронное выполнение
+                }.boxed()
+            });
+        rep.expect_persist_category_created_event()
+            .times(1) // Ожидаем, что будет вызван ровно один раз
+            .returning(|_| async { Ok(()) }.boxed());
+
+        let mut create_category_command_handler = CreateCategoryCommandHandler::new(rep);
+
+        let command = create_command_fixture();
+        let result = create_category_command_handler.handle(command).await;
+
+        assert!(result.is_ok());
+    }
+
+    #[tokio::test]
+    async fn test_create_category_command_handler_existing_category() {
+        let mut rep = MockCategoryRepository::new();
+        rep.expect_exists()
+            .times(1)
+            .returning(|_, _, _| async { Ok(true) }.boxed()); // Имитация существующей категории
+
+        let mut create_category_command_handler = CreateCategoryCommandHandler::new(rep);
+
+        let command = create_command_fixture();
+        let result = create_category_command_handler.handle(command).await;
+
+        assert!(matches!(result, Err(Error::Client(BadRequest { .. }))));
+    }
+
+    #[tokio::test]
+    async fn test_create_category_command_handler_repository_error() {
+        let mut rep = MockCategoryRepository::new();
+        rep.expect_exists()
+            .times(1)
+            .returning(|_, _, _| async { Err(Error::Server(InternalServerError { context: Some("DB error".into()) })) }.boxed());
+
+        let mut create_category_command_handler = CreateCategoryCommandHandler::new(rep);
+
+        let command = create_command_fixture();
+        let result = create_category_command_handler.handle(command).await;
+
+        assert!(matches!(result, Err(Error::Server(InternalServerError { .. }))));
+    }
+
+
+
+    fn create_command_fixture() -> CreateCategoryCommand {
+        CreateCategoryCommand::new(
+            Uuid::new_v4(),
+            "Test Category".to_string(),
+            None,
+        )
+    }
+}
